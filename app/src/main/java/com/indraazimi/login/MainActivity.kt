5c004186ffac57d65e12b9befbe43804d414238d
Indra Azimi <indraazimi@yahoo.com>
2020-09-15 13:12:54 +0700

    Menambahkan login dengan akun Google

--- app/src/main/java/com/indraazimi/login/MainActivity.kt
+++ app/src/main/java/com/indraazimi/login/MainActivity.kt
@@ -9,13 +9,49 @@
 
 package com.indraazimi.login
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val SIGN_IN_REQUEST_CODE = 101
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        login.setOnClickListener { mulaiLogin() }
+    }
+
+    private fun mulaiLogin() {
+        val providers = arrayListOf(AuthUI.IdpConfig.GoogleBuilder().build())
+        val intent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        startActivityForResult(intent, SIGN_IN_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == SIGN_IN_REQUEST_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                val nama = FirebaseAuth.getInstance().currentUser?.displayName
+                Log.i("LOGIN", "$nama berhasil login")
+            } else {
+                Log.i("LOGIN", "Login gagal: ${response?.error?.errorCode}")
+            }
+        }
     }
 }
