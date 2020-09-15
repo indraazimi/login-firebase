024dbf6ea00efc1db72e77a71387c74ffa5b0d3e
Indra Azimi <indraazimi@yahoo.com>
2020-09-15 14:05:43 +0700

    Update UI berdasarkan status login

--- app/src/main/java/com/indraazimi/login/MainActivity.kt
+++ app/src/main/java/com/indraazimi/login/MainActivity.kt
@@ -9,14 +9,12 @@
 
 package com.indraazimi.login
 
-import android.app.Activity
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.IdpResponse
-import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -30,28 +28,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         login.setOnClickListener { mulaiLogin() }
+
+        val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel.authState.observe(this, Observer { updateUI(it) })
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        login.text = if (user == null)
+            getString(R.string.login)
+        else
+            getString(R.string.logout)
     }
 
     private fun mulaiLogin() {
+        if (login.text == getString(R.string.logout)) {
+            AuthUI.getInstance().signOut(this)
+            return
+        }
+
         val providers = arrayListOf(AuthUI.IdpConfig.GoogleBuilder().build())
         val intent = AuthUI.getInstance()
             .createSignInIntentBuilder()
             .setAvailableProviders(providers)
             .build()
         startActivityForResult(intent, SIGN_IN_REQUEST_CODE)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == SIGN_IN_REQUEST_CODE) {
-            val response = IdpResponse.fromResultIntent(data)
-            if (resultCode == Activity.RESULT_OK) {
-                val nama = FirebaseAuth.getInstance().currentUser?.displayName
-                Log.i("LOGIN", "$nama berhasil login")
-            } else {
-                Log.i("LOGIN", "Login gagal: ${response?.error?.errorCode}")
-            }
-        }
     }
 }
---
+++ app/src/main/java/com/indraazimi/login/MainViewModel.kt
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2020 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 2.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.login
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    val authState = FirebaseUserLiveData()
+
+}
