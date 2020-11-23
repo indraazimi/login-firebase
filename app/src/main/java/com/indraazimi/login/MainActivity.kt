7f6ebab77ba54fc30b3d48e244742452a88d80f8
Indra Azimi <indraazimi@yahoo.com>
2020-11-23 21:03:35 +0700

    Membuat check-in dengan Custom Tabs

--- app/src/main/java/com/indraazimi/login/MainActivity.kt
+++ app/src/main/java/com/indraazimi/login/MainActivity.kt
@@ -9,9 +9,11 @@
 
 package com.indraazimi.login
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.firebase.ui.auth.AuthUI
@@ -21,6 +23,10 @@ import com.indraazimi.login.databinding.ActivityMainBinding
 import com.indraazimi.login.notify.AlarmUtils
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val CHECK_IN_URL = "https://checkin.telkomuniversity.ac.id"
+    }
 
     private val contract = FirebaseAuthUIActivityResultContract()
     private val signInLauncher = registerForActivityResult(contract) { }
@@ -37,6 +43,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.login.setOnClickListener { mulaiLogin() }
         binding.logout.setOnClickListener { AuthUI.getInstance().signOut(this) }
+        binding.checkin.setOnClickListener { checkInSekarang() }
 
         viewModel.authState.observe(this, { updateUI(it) })
     }
@@ -64,5 +71,10 @@ class MainActivity : AppCompatActivity() {
             .setAvailableProviders(providers)
             .build()
         signInLauncher.launch(intent)
+    }
+
+    private fun checkInSekarang() {
+        val intent = CustomTabsIntent.Builder().build()
+        intent.launchUrl(this, Uri.parse(CHECK_IN_URL))
     }
 }
