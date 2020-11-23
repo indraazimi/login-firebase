ce9475f851d1d09601bf31891374cf29dcfa652e
Indra Azimi <indraazimi@yahoo.com>
2020-11-23 12:22:32 +0700

    Menampilkan notifikasi check-in

--- app/src/main/java/com/indraazimi/login/MainActivity.kt
+++ app/src/main/java/com/indraazimi/login/MainActivity.kt
@@ -9,15 +9,18 @@
 
 package com.indraazimi.login
 
+import android.app.NotificationManager
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.google.firebase.auth.FirebaseUser
 import com.indraazimi.login.databinding.ActivityMainBinding
+import com.indraazimi.login.notify.sendNotification
 
 class MainActivity : AppCompatActivity() {
 
@@ -36,6 +39,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.login.setOnClickListener { mulaiLogin() }
         binding.logout.setOnClickListener { AuthUI.getInstance().signOut(this) }
+        binding.notify.setOnClickListener { tampilNotifikasi() }
 
         viewModel.authState.observe(this, { updateUI(it) })
     }
@@ -61,5 +65,14 @@ class MainActivity : AppCompatActivity() {
             .setAvailableProviders(providers)
             .build()
         signInLauncher.launch(intent)
+    }
+
+    private fun tampilNotifikasi() {
+        // Seharusnya notifikasi ditampilkan oleh proses di background,
+        // karena akan aneh jika notifikasi tampil saat aplikasi dibuka.
+        // Kita akan menangani masalah ini nanti.
+        val notificationManager = ContextCompat.getSystemService(
+            this, NotificationManager::class.java)
+        notificationManager?.sendNotification(this)
     }
 }
