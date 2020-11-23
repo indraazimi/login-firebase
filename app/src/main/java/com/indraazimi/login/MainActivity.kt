b52bb24b246d69f47dcd20ff7628812f81b77178
Indra Azimi <indraazimi@yahoo.com>
2020-11-23 20:38:03 +0700

    Trigger notifikasi dari background

--- app/src/main/java/com/indraazimi/login/MainActivity.kt
+++ app/src/main/java/com/indraazimi/login/MainActivity.kt
@@ -9,17 +9,15 @@
 
 package com.indraazimi.login
 
-import android.app.NotificationManager
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseUser
-import com.indraazimi.login.notify.sendNotification
+import com.indraazimi.login.notify.AlarmUtils
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -34,10 +32,11 @@ class MainActivity : AppCompatActivity() {
 
         login.setOnClickListener { mulaiLogin() }
         logout.setOnClickListener { AuthUI.getInstance().signOut(this) }
-        notify.setOnClickListener { tampilNotifikasi() }
 
         val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         viewModel.authState.observe(this, Observer { updateUI(it) })
+
+        AlarmUtils.setAlarm(this)
     }
 
     private fun updateUI(user: FirebaseUser?) {
@@ -61,14 +60,5 @@ class MainActivity : AppCompatActivity() {
             .setAvailableProviders(providers)
             .build()
         startActivityForResult(intent, SIGN_IN_REQUEST_CODE)
-    }
-
-    private fun tampilNotifikasi() {
-        // Seharusnya notifikasi ditampilkan oleh proses di background,
-        // karena akan aneh jika notifikasi tampil saat aplikasi dibuka.
-        // Kita akan menangani masalah ini nanti.
-        val notificationManager = ContextCompat.getSystemService(
-            this, NotificationManager::class.java)
-        notificationManager?.sendNotification(this)
     }
 }
---
+++ app/src/main/java/com/indraazimi/login/notify/AlarmUtils.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2020 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 2.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.login.notify
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import java.util.*
+
+object AlarmUtils {
+
+    private const val REQUEST_CODE = 1
+
+    fun setAlarm(context: Context) {
+        val intent = Intent(context, ReminderReceiver::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(context, REQUEST_CODE, intent, 0)
+        val manager = context.getSystemService(Context.ALARM_SERVICE) as? AlarmManager
+        manager?.setInexactRepeating(AlarmManager.RTC_WAKEUP, getTime(),
+            AlarmManager.INTERVAL_HALF_DAY, pendingIntent)
+    }
+
+    private fun getTime(): Long {
+        val calendar = Calendar.getInstance()
+
+        // Reminder akan diberikan 2x sehari, jam 8 pagi dan 8 malam
+        val jam = calendar.get(Calendar.HOUR_OF_DAY)
+        val waktu = if (jam in 8..19) 20 else 8
+        calendar.set(Calendar.HOUR_OF_DAY, waktu)
+        calendar.set(Calendar.MINUTE, 0)
+        calendar.set(Calendar.SECOND, 0)
+
+        // Jika jam hari ini sudah lewat, atur reminder buat besok
+        if (Calendar.getInstance().after(calendar)) {
+            calendar.add(Calendar.DATE, 1)
+        }
+        return calendar.timeInMillis
+    }
+}
