62c2d9a0d38c9352445467318af723293f20ab2f
Indra Azimi <indraazimi@yahoo.com>
2020-12-02 11:30:01 +0700

    Mengambil token FCM pada device pengguna

--- app/src/main/java/com/indraazimi/login/MainViewModel.kt
+++ app/src/main/java/com/indraazimi/login/MainViewModel.kt
@@ -9,10 +9,32 @@
 
 package com.indraazimi.login
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainViewModel : ViewModel() {
 
     val authState = FirebaseUserLiveData()
 
+    init {
+        // Untuk mengirim notifikasi ke user tertentu, kita butuh token.
+        // Method ini akan mengambil token yang sedang aktif saat ini.
+        checkToken()
+    }
+
+    private fun checkToken() {
+        val tokenTask = FirebaseMessaging.getInstance().token
+        tokenTask.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("FCM", "FCM token failed.", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Setelah mendapat token, biasanya kita mengirimkan data ini
+            // ke back-end server. Tapi di praktikum ini, kita log saja.
+            Log.d("FCM", "Token: ${task.result}")
+        })
+    }
 }
---
+++ app/src/main/java/com/indraazimi/login/notify/FcmService.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2021 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 2.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.login.notify
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class FcmService : FirebaseMessagingService() {
+
+    override fun onNewToken(token: String) {
+        // Ini akan dipanggil setiap kali token baru di-generate.
+        // Setelah mendapat token baru, biasanya kita mengirimnya
+        // ke back-end server. Tapi di praktikum ini, kita log saja.
+        Log.d("FCM", "Token baru: $token")
+    }
+}
